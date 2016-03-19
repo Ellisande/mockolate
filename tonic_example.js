8e9f2b404ba964366164d61a1213743f16bf7513
Ellisande <justin.dragos@gmail.com>
2016-03-18 20:16:40 -0700

    Added a tonic example.
    This will let people try mockolate in their browser via npmjs.org.

---
+++ tonic_example.js
@@ -0,0 +1,12 @@
+import {mockFunction} from 'mockolate';
+const ninja = {
+  getArsenal: mockFunction()
+};
+
+ninja.getArsenal.when('ninja stars').then.return('100 crazy awesome ninja stars');
+
+console.log(ninja.getArsenal('ninja stars'))
+//Result '100 crazy awesome ninja stars'
+
+console.log(ninja.getArsenal());
+//Result undefined, since we didn't specify a when for no arguments
