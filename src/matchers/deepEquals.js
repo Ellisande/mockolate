3784307e7290f8e9dd3a03a70eee1f9ff8d442c2
David Bradford <david.bradford@yodle.com>
2016-10-02 19:42:16 -0500

    Add deepEquals matcher

---
+++ src/matchers/deepEquals.js
@@ -0,0 +1,20 @@
+import Matcher from './matcher';
+import _ from 'lodash';
+
+class DeepEqualsMatcher extends Matcher {
+    constructor(value){
+        super();
+        this.value = value;
+    }
+    matches(otherValue) {
+        if (_.isNull(this.value) || _.isUndefined(this.value)) {
+            return false;
+        }
+        return _.isEqual(this.value, otherValue);
+    }
+}
+
+export {
+    DeepEqualsMatcher
+};
+export default (valueToMatch) => new DeepEqualsMatcher(valueToMatch);
